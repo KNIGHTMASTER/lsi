7a0787550b0222f17c4719f427f307fd881e36ca
KNIGHTMASTER <fauzi.knightmaster.achmad@gmail.com>
2018-09-10 07:42:23 +0700

    Initial Commit

---
+++ lsi-core/src/main/java/com/tripoin/lsi/core/LsiCoreApplication.java
@@ -0,0 +1,17 @@
+package com.tripoin.lsi.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created on 9/10/18.
+ *
+ * @author <a href="mailto:fauzi.knightmaster.achmad@gmail.com">Achmad Fauzi</a>
+ */
+@SpringBootApplication
+public class LsiCoreApplication {
+
+    public static void main(String [] args) {
+        SpringApplication.run(LsiCoreApplication.class);
+    }
+}
